e50b01aa2ba0dc9a9ff49f2f98dcb83ff9b39947
Aaron Hooper <rnjckhpr@gmail.com>
2017-09-12 22:15:48 +0100

    Add placeholder text to search box

--- src/components/Search.js
+++ src/components/Search.js
@@ -12,7 +12,8 @@ class Search extends Component {
         <form className="form" onSubmit={this.props.onSubmit}>
           <div className="input-group">
             <input className="form-control" id="search" type="search"
-                   onChange={this.props.onChange}>
+                   onChange={this.props.onChange}
+                   placeholder="Name of artist, musician, band...">
             </input>
             <span className="input-group-btn">
               <button className="btn btn-primary" id="submit" type="submit">Go!</button>
