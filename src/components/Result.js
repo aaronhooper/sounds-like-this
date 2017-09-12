b4f7642b4d0a3f5d5596c0ec719ce6813082b4ef
Aaron Hooper <rnjckhpr@gmail.com>
2017-09-12 19:01:25 +0100

    Add placeholder image for every artist with none

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react'
 
 class Result extends Component {
-  
+
   constructor(props) {
     super(props)
+  }
+
+  insertPlaceholder() {
+    if (!this.props.img) return './noimage.png'
+    return this.props.img
   }
 
   render() {
     return (
       <h3 className="result">
-        <img src={this.props.img} alt="" />
+        <img src={this.insertPlaceholder()} alt="" />
         <a href={this.props.href}>
           {this.props.name}
         </a>
