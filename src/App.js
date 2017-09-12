84f7c9288dde912e02a8a9849acacc0b9efae3b3
Aaron Hooper <rnjckhpr@gmail.com>
2017-09-12 18:11:06 +0100

    Change component structure

--- src/App.js
+++ src/App.js
@@ -1,20 +1,87 @@
 import React, { Component } from 'react'
 import Search from './components/Search'
-
+import Result from './components/Result'
+import axios from 'axios'
+import url from 'url'
 import './App.css'
 
 class App extends Component {
-  // constructor() {
-  //   super()
-  // }
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      response: {}
+    }
+
+    this.getData = this.getData.bind(this)
+    this.updateSearchTerm = this.updateSearchTerm.bind(this)
+  }
+
+  getData(e) {
+    e.preventDefault()
+    const apiHref = 'http://ws.audioscrobbler.com/2.0/'
+    let apiURLObject = url.parse(apiHref)
+
+    apiURLObject.query = {
+      method: 'artist.getsimilar',
+      api_key: '9a9863a1638f78d7be214bb63d0c0ea0',
+      format: 'json',
+      limit: '8',
+      artist: this.state.searchTerm
+    }
+
+    axios.get(url.format(apiURLObject))
+         .then(res => {
+           console.log(url.format(apiURLObject))
+           this.setState({ response: res })
+          //  console.dir(this.state.response)
+         })
+         .catch(err => { console.log(err) })
+  }
+
+  updateSearchTerm(e) {
+    this.setState({ searchTerm: e.target.value })
+  }
+
+  renderResults() {
+    if (!this.state.response) {
+      console.log('Response doesn\'t exist!')
+      console.dir(this.state.response)
+      return
+    }
+
+    try {
+      if (this.state.response.data.error === 6) {
+        console.log('None found!')
+        return
+      }
+    } catch (e) {
+      console.log('Does not exist')
+      return
+    }
+
+    let results = []
+    let artists = this.state.response.data.similarartists.artist
+
+    for (let i = 0; i < artists.length; i++) {
+      results.push(
+        <Result img={artists[i].image[1]['#text']}
+                href={artists[i].url}
+                name={artists[i].name}
+        />
+      )
+    }
+    return results
+  }
 
   render() {
     return (
       <div className="App container">
         <div className="row">
-          <div className="col-md-6 col-centered">
+          <div className="col-lg-6 col-centered">
             <h1>sounds like this</h1>
-            <Search />
+            <Search onSubmit={this.getData} onChange={this.updateSearchTerm} />
+            {this.renderResults()}
           </div>
         </div>
       </div>
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,83 +1,27 @@
 import React, { Component } from 'react'
-import Result from './Result'
-import axios from 'axios'
-import url from 'url'
 
 class Search extends Component {
 
   constructor(props) {
     super(props)
-    this.state = {
-      searchTerm: ""
-    }
-    this.getData = this.getData.bind(this)
-    this.updateSearchTerm = this.updateSearchTerm.bind(this)
-  }
 
-  getData(e) {
-    e.preventDefault()
-    const apiHref = 'http://ws.audioscrobbler.com/2.0/'
-    let apiURLObject = url.parse(apiHref)
-
-    apiURLObject.query = {
-      method: 'artist.getsimilar',
-      api_key: '9a9863a1638f78d7be214bb63d0c0ea0',
-      format: 'json',
-      limit: '8',
-      artist: this.state.searchTerm
-    }
-
-    axios.get(url.format(apiURLObject))
-         .then(res => {
-           this.setState({ response: res })
-         })
-         .catch(err => { console.log(err) })
-  }
-
-  renderResults() {
-    if (!this.state.response) return
-
-    if (this.state.response.data.error === 6) {
-      console.log('None found!')
-      return
-    }
-
-    let results = []
-    let artists = this.state.response.data.similarartists.artist
-
-    for (let i = 0; i < artists.length; i++) {
-      results.push(
-        <Result img={artists[i].image[1]['#text']}
-                href={artists[i].url}
-                name={artists[i].name}
-        />
-      )
-    }
-
-    return results
-  }
-
-  updateSearchTerm(e) {
-    this.setState({ searchTerm: e.target.value })
+    this.state = {}
   }
 
   render() {
     return (
       <div>
-        <form className="form" onSubmit={this.getData}>
+        <form className="form" onSubmit={this.props.onSubmit}>
           <div className="input-group">
             <input className="form-control" id="search" type="search"
                    value={this.state.searchTerm}
-                   onChange={this.updateSearchTerm}>
+                   onChange={this.props.onChange}>
             </input>
             <span className="input-group-btn">
               <button className="btn btn-primary" id="submit" type="submit">Go!</button>
             </span>
           </div>
         </form>
-        <div>
-          {this.renderResults()}
-        </div>
       </div>
     )
   }
